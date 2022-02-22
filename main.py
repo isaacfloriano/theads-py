10fdc02aa7e4653e89d165758033a653c2d8e102
Isaac Silva <isaacandroid180320@gmail.com>
2022-02-22 00:17:33 -0300

    main

--- main.py
+++ main.py
@@ -11,11 +11,7 @@ carga = input("Quantidade de clicks: ")
 link = input("Cole o link aqui: ")
 contador = 0
 while(contador <= int(carga)):
-    #link = 'http://localhost/dashboard/web/2022/meus/flodar/processa_vis.php'
     t = threading.Thread(target=flood,args=(link,))
     print("Click "+str(contador))
     t.start()
     contador = contador + 1
-
-# Artigo de referencia
-# https://alissonmachado.com.br/python-threads#:~:text=Threads%20s%C3%A3o%20uma%20forma%20de,como%20por%20exemplo%20o%20Apache.
