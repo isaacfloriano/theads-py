f9b81a879106ea4c8bb57656e0c6e711ebcdb5bf
Isaac Silva <isaacandroid180320@gmail.com>
2022-02-23 12:54:11 -0300

    main

--- visita.php
+++ visita.php
@@ -1,19 +1,32 @@
 <?php
+error_reporting(0);
+
+if (isset($_POST['limpar'])) {
+	# limpar o txt
+	fopen('contador.txt', "w+");
+}
 if (file_exists("contador.txt") !== false) {
 	$letra = 'r';
-}else{
+} else {
 	$letra = 'w';
 }
+
 $arquivo = 'contador.txt';
-$handle = fopen($arquivo,"$letra+");
+$handle = fopen($arquivo, "$letra+");
 
 $data = fread($handle, 512);
 $contador = $data + 1;
 
-echo $contador;
+echo "<center><h3>" . $contador . "</h3>";
 
 fseek($handle, 0);
 
 fwrite($handle, $contador);
 
 fclose($handle);
+
+# Index botões
+echo "<form method='post'>
+		<button type = 'submit'>Atualizar</button>
+		<button type = 'submit' name = 'limpar'>Limpar</button>
+	</form>";
